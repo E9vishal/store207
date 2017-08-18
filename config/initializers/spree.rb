2d9839679249c738b8ca2ddc2156e8f01b1bb063
vishal <vishalbiradar205@gmail.com>
2017-08-18 15:18:17 +0530

    added E9logo to mystore

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,6 +13,8 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+  config.logo = 'E9_Consulting_Logo.jpg'
+  
 end
 
 Spree.user_class = "Spree::User"
